4bfd2b53a85e13f557229e8132a4b30f793545bf
bennytemmerman <81377775+bennytemmerman@users.noreply.github.com>
2025-06-05 08:41:21 +0200

    Update main.go

--- main.go
+++ main.go
@@ -1,71 +1,35 @@
-// main.go
 package main
 
 import (
 	"fmt"
-	"net/url"
 	"os"
-	"sync"
-	"strconv"
 )
 
 func main() {
-	if len(os.Args) != 4 {
-		fmt.Println("usage: ./crawler URL maxConcurrency maxPages")
-		os.Exit(1)
+	if len(os.Args) < 2 {
+		fmt.Println("no website provided")
+		return
+	}
+	if len(os.Args) > 2 {
+		fmt.Println("too many arguments provided")
+		return
 	}
 	rawBaseURL := os.Args[1]
-	maxConcurrency, err := strconv.Atoi(os.Args[2])
+
+	const maxConcurrency = 3
+	cfg, err := configure(rawBaseURL, maxConcurrency)
 	if err != nil {
-		fmt.Println("Invalid maxConcurrency:", os.Args[2])
-		os.Exit(1)
-	}
-	maxPages, err := strconv.Atoi(os.Args[3])
-	if err != nil {
-		fmt.Println("Invalid maxPages:", os.Args[3])
-		os.Exit(1)
+		fmt.Printf("Error - configure: %v", err)
+		return
 	}
 
-	parsedBase, err := url.Parse(rawBaseURL)
-	if err != nil {
-		fmt.Printf("Invalid URL: %v\n", err)
-		os.Exit(1)
-	}
-	fmt.Println("Parsed base URL:", parsedBase.String())
-	cfg := &config{
-		pages:              make(map[string]int),
-		baseURL:            parsedBase,
-		mu:                 &sync.Mutex{},
-		concurrencyControl: make(chan struct{}, maxConcurrency),
-		wg:                 &sync.WaitGroup{},
-		maxPages:           maxPages,
-	}
+	fmt.Printf("starting crawl of: %s...\n", rawBaseURL)
 
 	cfg.wg.Add(1)
-	cfg.concurrencyControl <- struct{}{} // use a slot
 	go cfg.crawlPage(rawBaseURL)
 	cfg.wg.Wait()
 
-	args := os.Args[1:]
-
-	fmt.Println("\nCrawled Pages Report:")
-	for url, count := range cfg.pages {
-		fmt.Printf("%s — visited %d time(s)\n", url, count)
-	}
-
-	fmt.Println("\nPages visited:")
-	for url, count := range cfg.pages {
-		fmt.Printf("%s — visited %d time(s)\n", url, count)
-	}
-
-	baseURL := args[0]
-	fmt.Printf("starting crawl of: %s\n", baseURL)
-
-	pages := make(map[string]int)
-	crawlPage(baseURL, baseURL, pages)
-
-	fmt.Println("\nCrawled pages:")
-	for url, count := range pages {
-		fmt.Printf("%s: %d\n", url, count)
+	for normalizedURL, count := range cfg.pages {
+		fmt.Printf("%d - %s\n", count, normalizedURL)
 	}
 }
