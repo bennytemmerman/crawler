f0b898e4a68973cdb251d96753076e4dcd244036
bennytemmerman <81377775+bennytemmerman@users.noreply.github.com>
2025-06-05 08:41:30 +0200

    Update normalize_url.go

--- normalize_url.go
+++ normalize_url.go
@@ -1,35 +1,22 @@
-// normalize_url.go
 package main
 
 import (
+	"fmt"
 	"net/url"
 	"strings"
 )
 
-// normalizeURL takes a URL string and returns a normalized version of it.
 func normalizeURL(rawURL string) (string, error) {
 	parsedURL, err := url.Parse(rawURL)
 	if err != nil {
-		return "", err
+		return "", fmt.Errorf("couldn't parse URL: %w", err)
 	}
 
-	host := strings.ToLower(parsedURL.Host)
-	if strings.HasPrefix(host, "www.") {
-		host = strings.TrimPrefix(host, "www.")
-	}
+	fullPath := parsedURL.Host + parsedURL.Path
 
-	path := parsedURL.EscapedPath()
-	if path == "/" {
-		path = ""
-	}
+	fullPath = strings.ToLower(fullPath)
 
-	normalized := host + path
+	fullPath = strings.TrimSuffix(fullPath, "/")
 
-	// Include query string if it exists
-	if parsedURL.RawQuery != "" {
-		normalized += "?" + parsedURL.RawQuery
-	}
-
-	return normalized, nil
+	return fullPath, nil
 }
-
